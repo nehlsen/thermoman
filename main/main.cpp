abc5d35b19c998691d630d07e9339c859d372224
Nils Weigel <nils.weigel@gmail.com>
2020-10-24 15:44:03 +0200

    initial work on ThermoMan

---
+++ main/main.cpp
@@ -0,0 +1,16 @@
+#include <ebli.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void app_main()
+{
+    EBLi::init_all();
+
+    // my awesome project starts here
+}
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
