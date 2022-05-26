615c691d6cbc8bb7f6976aa50985a183ca6bccde
Nils Weigel <nils.weigel@gmail.com>
2022-05-26 15:17:18 +0200

    application logic in application not in overcomplicated lib config

--- main/main.cpp
+++ main/main.cpp
@@ -1,4 +1,30 @@
 #include <ebli.h>
+#include <ebli_events.h>
+#include <esp_log.h>
+#include <Sensors.h>
+#include <PowerManager.h>
+
+#define SLEEP_TIME_IN_SECONDS 10
+
+static void readAndPublishSensorsAndWaitAndSleep()
+{
+    ESP_LOGI("APP", "read Sensors");
+    EBLi::Sensors::Sensors::publishAll();
+
+    ESP_LOGI("APP", "wait a sec");
+    // wait one second to give sensors time to finish its job
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    ESP_LOGI("APP", "request sleep");
+    auto pm = EBLi::Power::PowerManager::instance();
+    pm->requestWakeupIn(SLEEP_TIME_IN_SECONDS * 1000);
+}
+
+static void onEbliMqttConnected(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    ESP_LOGI("APP", "onEbliMqttConnected");
+    readAndPublishSensorsAndWaitAndSleep();
+}
 
 #ifdef __cplusplus
 extern "C" {
@@ -8,7 +34,9 @@ void app_main()
 {
     EBLi::init_all();
 
-    // my awesome project starts here
+    ESP_ERROR_CHECK(esp_event_handler_register(EBLI_EVENTS, EBLI_EVENT_MQTT_CONNECTED, onEbliMqttConnected, nullptr));
+
+    readAndPublishSensorsAndWaitAndSleep();
 }
 
 #ifdef __cplusplus
